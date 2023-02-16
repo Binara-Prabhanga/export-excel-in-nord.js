f68c12521eccae2c2710bd47f39e0b0d2de14662
Prabhanga K. G. B. it21211096 <it21211096@my.sliit.lk>
2023-02-16 09:48:34 +0530

    Export Excel

---
+++ Models/User.js
@@ -0,0 +1,28 @@
+// Get data from database
+
+// Sample data
+
+const User = [
+  { fname: "Joe", lname: "Sia", email: "joe@gmail.com", gender: "Male" },
+  {
+    fname: "Anuga",
+    lname: "Senarath",
+    email: "anu@gmail.com",
+    gender: "Male",
+  },
+  { fname: "Nijah", lname: "Senth", email: "nijah@gmail.com", gender: "Female" },
+  {
+    fname: "Randew",
+    lname: "Mewran",
+    email: "randr@gmail.com",
+    gender: "Male",
+  },
+  {
+    fname: "Van",
+    lname: "Siri",
+    email: "siri@gmail.com",
+    gender: "Female",
+  },
+];
+
+module.exports = User;
